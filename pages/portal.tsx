611ee18adbca10124a90b67eacb43081625a7680
Valeri Sabev <valchygaming@gmail.com>
2022-06-26 18:18:08 +0200

    Portal page wip

--- pages/portal.tsx
+++ pages/portal.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next';
 import Layout from '@components/Layout';
-import GoogleButton from '@components/GoogleButton';
+import GoogleButton from '@components/buttons/GoogleButton';
+import GithubButton from '@components/buttons/GithubButton';
 
 const GoCode: NextPage = () => {
 	return (
 		<Layout>
-			<GoogleButton />
+			<main className="flex flex-col justify-center items-center h-96">
+				<h1 className="text-2xl mb-12">Please choose a method to sign-in</h1>
+				<GoogleButton />
+				<span className="my-2">or</span>
+				<GithubButton />
+			</main>
 		</Layout>
 	);
 };
