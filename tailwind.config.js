2c729c47d0ee7e8623c95ed0d48c33a7aa848e29
Valeri Sabev <valchygaming@gmail.com>
2022-07-01 12:30:49 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		screens: {
+			sm: '680px',
+			md: '850px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		}
 	},
 	plugins: []
 };
