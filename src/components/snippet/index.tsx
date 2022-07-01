b41418ea0d35fb325ab24050d511e947b52c2287
Valeri Sabev <valchygaming@gmail.com>
2022-07-01 15:45:27 +0100

    Update index.tsx

--- src/components/snippet/index.tsx
+++ src/components/snippet/index.tsx
@@ -14,22 +14,24 @@ type SnippetType = {
 };
 
 export default function Snippets({ snippets = [] }: Snippets) {
-	// Show react skeleton loader if no snippets
-	if (snippets.length === 0)
-		return (
-			<div>
-				<Skeleton width={300} />
-				<Skeleton width={300} />
-				<Skeleton width={300} />
-				<br />
-				<Skeleton width={300} />
-			</div>
-		);
-	else
-		return snippets.map(({ author, code_snippet, code_language, _id }) => (
-			<div key={_id} className="mb-3">
-				<CopyBlock text={code_snippet} language={code_language} showLineNumbers={10} theme={vs2015} codeBlock />
-				<span className="mt-3">Code Snippet by: {author}</span>
-			</div>
-		));
+	return (
+		<>
+			{snippets.length === 0 ? (
+				<div>
+					<Skeleton width={300} />
+					<Skeleton width={300} />
+					<Skeleton width={300} />
+					<br />
+					<Skeleton width={300} />
+				</div>
+			) : (
+				snippets.map(({ author, code_snippet, code_language, _id }) => (
+					<div key={_id} className="mb-3">
+						<CopyBlock text={code_snippet} language={code_language} showLineNumbers={10} theme={vs2015} codeBlock />
+						<span className="mt-3">Code Snippet by: {author}</span>
+					</div>
+				))
+			)}
+		</>
+	);
 }
