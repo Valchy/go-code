58b156c3eafc6f9828d2f8da4ae0cb4a69ae69bd
Valeri Sabev <valchygaming@gmail.com>
2022-07-01 16:31:02 +0100

    search

---
+++ pages/api/snippets/all.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import connectMongo from '@utils/mongoose';
+import Snippet from '@models/Snippet';
+
+type SnippetType = {
+	name: string;
+};
+
+type ErrorType = {
+	error: any;
+	data: [];
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<SnippetType[] | ErrorType>) {
+	try {
+		await connectMongo();
+
+		const snippets = await Snippet.find();
+		res.status(200).json(snippets);
+	} catch (error) {
+		res.json({ error, data: [] });
+	}
+}
--- pages/snippets/search.tsx
+++ pages/snippets/search.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
 import Layout from '@components/Layout';
+import Snippets from '@components/snippet';
+
+type SnippetType = {
+	author: string;
+	snippet_title: string;
+	code_snippet: string;
+	code_language: string;
+	_id: string;
+};
 
 export default function Search() {
-	return <Layout>Working on it...</Layout>;
+	const [snippets, setSnippets] = useState<SnippetType[]>([]);
+
+	useEffect(() => {
+		setTimeout(
+			() =>
+				fetch(`/api/snippets/all`)
+					.then(res => res.json())
+					.then((snippets: SnippetType[]) => setSnippets(snippets)),
+			1000
+		);
+	}, []);
+
+	return (
+		<Layout>
+			<div className="flex flex-wrap justify-around">
+				<Snippets snippets={snippets} />
+			</div>
+		</Layout>
+	);
 }
